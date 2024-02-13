5a66407b8f0da3fe6c5efa559e56b0a996eff1d5
James Bland <jammydodger8888@gmail.com>
2024-02-13 16:26:44 +0000

    Refarctored ready to pickup code

--- Assets/Scripts/CameraForwardsSampler.cs
+++ Assets/Scripts/CameraForwardsSampler.cs
@@ -5,72 +5,92 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Serialization;
 
-namespace UFO
-{
-    [System.Serializable]
-    public class UiAlert : UnityEvent
-    {
-        
-    };
-
     [RequireComponent(typeof(Camera))]
 public class CameraForwardsSampler : MonoBehaviour
 {
     private UnityEngine.Camera camera;
-    public UiAlert PickupableObjectInRangeUnityEvent;
-    public UiAlert PickupableObjectOutOfRangeUnityEvent;
-
     private bool pickupableObjectInRange = false;
+    
+    [SerializeField]
+    private UFO.UiAlert PickupableObjectInRangeUnityEvent;
+    [SerializeField]
+    private UFO.UiAlert PickupableObjectOutOfRangeUnityEvent;
+    
 
     private bool PickupableObjectInRange
     {
         set
         {
+            //if the value hasn't changed from last time then do nothing
             if (this.pickupableObjectInRange == value)
                 return;
-
+            
             if (value == true)
             {
+                //tell UI that your ready to pick up object
                 this.PickupableObjectInRangeUnityEvent.Invoke();
             }
             else
             {
+                //tell Ui that your not ready to pick up object
                 this.PickupableObjectOutOfRangeUnityEvent.Invoke();
             }
             
+            //finally set the value
             pickupableObjectInRange = value;
         }
     }
 
-    public void Awake()
+    void Start()
+    {
+        SendFirstUIEvents();
+        AssignCameraReference();
+    }
+
+    
+    //this is necessary becase when these events are first
+    //sent the camera tweaks out
+    private void SendFirstUIEvents()
     {
         this.PickupableObjectInRangeUnityEvent.Invoke();
         this.PickupableObjectOutOfRangeUnityEvent.Invoke();
     }
 
-    void Start()
+    private void AssignCameraReference()
     {
-        
         camera = gameObject.GetComponent<Camera>();
     }
 
     private void FixedUpdate()
     {
+        AreYouInRangeToPickupAnObject();
+    }
+
+    private void AreYouInRangeToPickupAnObject()
+    {
+        //setup variables
         this.PickupableObjectInRange = false;
         UnityEngine.RaycastHit hit;
+
         bool raycastCollision = Physics.Raycast(this.camera.transform.position, this.camera.transform.forward, out hit);
 
+        if (this.ReadyToPickupObject(hit, raycastCollision))
+            this.PickupableObjectInRange = true;
+    }
+
+    private bool ReadyToPickupObject(UnityEngine.RaycastHit hit, bool raycastCollision)
+    {
         if (!raycastCollision)
-            return;
+            return false;
 
         if (!hit.rigidbody)
-            return;
+            return false;
 
         if (!hit.rigidbody.gameObject is UFO.I_Pickupable)
-            return;
+            return false;
 
-        this.PickupableObjectInRange = true;
+        return true;
     }
 }
     
-}
+
