271287bbdcc10de709a1c8168baa32f3aa2c99f4
James Bland <jammydodger8888@gmail.com>
2024-02-14 17:33:41 +0000

    Added skeleton bones to the project to replace the cubes previously there

--- Assets/Scripts/Pickup Stuff/Bone.cs
+++ Assets/Scripts/Pickup Stuff/Bone.cs
@@ -1,10 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace UFO{
+    [ExecuteInEditMode]
 public class Bone : MonoBehaviour, UFO.I_Pickupable
 {
-}
+    private void OnValidate()
+    {
+        MeshCollider meshCollider = this.GetComponent<MeshCollider>();
 
+        meshCollider.sharedMesh = this.GetComponent<MeshFilter>().mesh;
+    }
+}
 }
