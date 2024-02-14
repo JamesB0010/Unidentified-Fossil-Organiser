e9fb8335376315241953b01d9abd58d39c43776d
James Bland <jammydodger8888@gmail.com>
2024-02-14 00:51:44 +0000

    Added Main Menu

---
+++ Assets/Scripts/Ui Stuff/MainMenuUi.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUi : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
