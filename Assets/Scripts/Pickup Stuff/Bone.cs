f1751afb2dc7f54df19d684789e536bff648aebb
James Bland <jammydodger8888@gmail.com>
2024-02-15 01:50:18 +0000

    Made Bones make a sound when colliding with something

--- Assets/Scripts/Pickup Stuff/Bone.cs
+++ Assets/Scripts/Pickup Stuff/Bone.cs
@@ -7,11 +7,22 @@ namespace UFO{
     [ExecuteInEditMode]
 public class Bone : MonoBehaviour, UFO.I_Pickupable
 {
+    private AudioSource audioSource;
     private void OnValidate()
     {
         MeshCollider meshCollider = this.GetComponent<MeshCollider>();
 
         meshCollider.sharedMesh = this.GetComponent<MeshFilter>().sharedMesh;
+    }
+
+    private void Start()
+    {
+        this.audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        this.audioSource.Play();
     }
 }
 }
---
+++ Assets/Scripts/Player Stuff/HeadBobController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//credit for headbob https://www.youtube.com/watch?v=5MbR2qJK8Tc
+public class HeadBobController : MonoBehaviour
+{
+
+}
