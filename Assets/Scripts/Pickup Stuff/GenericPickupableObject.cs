52810263623edcf007ef024e8f144d4d3a8c7183
James Bland <jammydodger8888@gmail.com>
2024-02-19 21:38:19 +0000

    Added generic Pickupable object script and attached it to a box in tylers whitebox scene

---
+++ Assets/Scripts/Pickup Stuff/GenericPickupableObject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UFO_PickupStuff;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GenericPickupableObject : MonoBehaviour, UFO_PickupStuff.I_Pickupable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
