2afc6124b79ac35ba52819f68eba7b411f8e6cc7
James Bland <jammydodger8888@gmail.com>
2024-02-20 16:18:31 +0000

    Added cool stuff like a desk, sofa and vent

--- Assets/Scripts/Pickup Stuff/GenericPickupableObject.cs
+++ Assets/Scripts/Pickup Stuff/GenericPickupableObject.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UFO_PickupStuff;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody)), RequireComponent(typeof(MeshCollider))]
 public class GenericPickupableObject : MonoBehaviour, UFO_PickupStuff.I_Pickupable
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponent<MeshCollider>().convex = true;
     }
 
     // Update is called once per frame
