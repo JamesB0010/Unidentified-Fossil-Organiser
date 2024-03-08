9689ebb6342773a613c124c722d6fc17c82f2f5b
James Bland <jammydodger8888@gmail.com>
2024-03-08 20:48:35 +0000

    Refactoring for tut

--- Assets/Scripts/GameSystems/LerpPackageProcessor.cs
+++ Assets/Scripts/GameSystems/LerpPackageProcessor.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using LerpData;
 using UnityEngine;
 
-class LerpPackageProcessor <ListType>
+class LerpPackageProcessor <CustomComponent>
 {
     #region Attributes
     //define a callback type for when a package has been processed
-    public delegate void PackageProcessed(ObjectLerpPackage<ListType> pkg);
+    public delegate void PackageProcessed(ObjectLerpPackage<CustomComponent> pkg);
     
     //queue data
-    private List<ObjectLerpPackage<ListType>> packageList = new List<ObjectLerpPackage<ListType>>();
+    private List<ObjectLerpPackage<CustomComponent>> packageList = new List<ObjectLerpPackage<CustomComponent>>();
     
     //lerping data
     private const float moveTowardsTarget = 1.0f;
@@ -18,7 +18,7 @@ class LerpPackageProcessor <ListType>
     #endregion
 
     #region Methods
-    public void AddPackage(ObjectLerpPackage<ListType> newPackage)
+    public void AddPackage(ObjectLerpPackage<CustomComponent> newPackage)
     {
         this.packageList.Add(newPackage);
     }
@@ -32,21 +32,24 @@ class LerpPackageProcessor <ListType>
     }
 
 
-    private void ProcessPackage(ObjectLerpPackage<ListType> pkg, int i)
+    private void ProcessPackage(ObjectLerpPackage<CustomComponent> pkg, int i)
     {
         LerpPackagePositionRotation(pkg);
         RemovePackageAtIndexIfCompleted(pkg, i);
     }
+
+
+    private void RemovePackageAtIndexIfCompleted(ObjectLerpPackage<CustomComponent> pkg, PackageProcessed packageProcessedCallBack, int i)
     
     private void RemovePackageAtIndexIfCompleted(ObjectLerpPackage<ListType> pkg, int i)
     {
-        if (pkg.current == 1)
+        if ((float)pkg.current == 1)
         {
             this.packageList.RemoveAt(i);
             pkg.callback(pkg);
         }
     }
-    private void LerpPackagePositionRotation(ObjectLerpPackage<ListType> pkg)
+    private void LerpPackagePositionRotation(ObjectLerpPackage<CustomComponent> pkg)
     {
         updateCurrentLerpPercentage(pkg);
 
@@ -55,11 +58,11 @@ class LerpPackageProcessor <ListType>
         LerpRotation(pkg);
     }
 
-    private void updateCurrentLerpPercentage(LerpData.ObjectLerpPackage<ListType> pkg)
+    private void updateCurrentLerpPercentage(LerpData.ObjectLerpPackage<CustomComponent> pkg)
     {
         pkg.current = Mathf.MoveTowards(pkg.current, moveTowardsTarget, pkg.lerpSpeed * Time.deltaTime);
     }
-    private static void LerpPosition(LerpData.ObjectLerpPackage<ListType> pkg)
+    private static void LerpPosition(LerpData.ObjectLerpPackage<CustomComponent> pkg)
     {
         pkg.objectToLerp.transform.position = Vector3.Lerp(pkg.start.position, pkg.target.position, pkg.current);
     }
--- Assets/Scripts/GameSystems/ObjectLerpPackage.cs
+++ Assets/Scripts/GameSystems/ObjectLerpPackage.cs
@@ -1,41 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 
 namespace LerpData
 {
-    public struct PositionRotationPair
+
+    abstract class ObjectLerpPackage<CustomComponent>
     {
-        public Vector3 position;
-        public Vector3 rotation;
+        public GameObject objectToLerp;
+        public CustomComponent customComponent;
+        public float lerpSpeed;
+        public Rigidbody rb = null;
+        public float current = 0.0f;
+        
+        public abstract object start { get; set; }
+        public abstract object target { get; set; }
     }
     
-class ObjectLerpPackage <CustomComponent>
+class FloatLerpPackage<CustomComponent>: ObjectLerpPackage<CustomComponent>
 {
+    #region attributes
 
-    #region Attributes
+    private float _start;
+    private float _target;
+
+    public override object start
+    {
+        get => this._start;
+        set => this._start = (float)value;
+    }
+
+    public override object target
+    {
+        get => this._target;
+        set => this._target = (float)value;
+    }
+    #endregion
     
-    public GameObject objectToLerp;
+    #region Methods
+    public FloatLerpPackage(float start,
+        float target, float lerpSpeed = 1.0f, GameObject objectToLerp = null)
+    {
+        this.objectToLerp = objectToLerp;
+        this.start = start;
+        this.target = target;
+        this.rb = this.objectToLerp.GetComponent<Rigidbody>();
+        this.customComponent = this.objectToLerp.GetComponent<CustomComponent>();
+        this.lerpSpeed = lerpSpeed;
+    }
+    #endregion
+}
 
-    public PositionRotationPair start;
-    public PositionRotationPair target;
+class Vector3LerpPackage<CustomComponent>: ObjectLerpPackage<CustomComponent>
+{
+    #region attributes
 
-    public LerpPackageProcessor<CustomComponent>.PackageProcessed callback;
-    
-    public Rigidbody rb;
-    
-    public CustomComponent customComponent;
+    private Vector3 _start;
+    private Vector3 _target;
 
-    public float current = 0.0f;
+    public override object start
+    {
+        get => this._start;
+        set => this._start = (Vector3)value;
+    }
 
-    public float lerpSpeed;
-    
+    public override object target
+    {
+        get => this._target;
+        set => this._target = (Vector3)value;
+    }
     #endregion
 
-    #region Methods
-    public ObjectLerpPackage(GameObject objectToLerp, PositionRotationPair start,
-        PositionRotationPair target, LerpPackageProcessor<CustomComponent>.PackageProcessed callback = null, float lerpSpeed = 1.0f)
+    Vector3LerpPackage(Vector3 start, Vector3 target, float lerpSpeed = 1.0f, GameObject objectToLerp = null)
     {
         this.objectToLerp = objectToLerp;
         this.start = start;
@@ -45,6 +83,5 @@ class ObjectLerpPackage <CustomComponent>
         this.lerpSpeed = lerpSpeed;
         this.callback = callback;
     }
-    #endregion
 }
 }
