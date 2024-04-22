2c25c607d73d7b84fd5ba373321ddbe343906cb0
James Bland <jammydodger8888@gmail.com>
2024-04-22 19:52:11 +0100

    Fixed lazer gun errors and made end stats update just need to fix error where you can refil the same sink or flush the same toilet multiple times and it be recorded more than once

--- Assets/Scripts/GameSystems/EasterEggManager.cs
+++ Assets/Scripts/GameSystems/EasterEggManager.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
 
 public class EasterEggManager : MonoBehaviour
 {
@@ -9,14 +12,32 @@ public class EasterEggManager : MonoBehaviour
 
     private bool shuttersOpen = false;
 
-    private int fullSinkCount = 0;
+    public int fullSinkCount = 0;
+
+    public int toiletsFlushed = 0;
+
+    public int sinksFilled = 0;
+
+    public int secretRoomsFound = 0;
 
     public UnityEvent OpenShuttersEvent = new UnityEvent();
 
     public UnityEvent CloseShuttersEvent = new UnityEvent();
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        SceneManager.activeSceneChanged += (arg0, scene) =>
+        {
+            EndScreenUiScript endScreen = FindObjectOfType<EndScreenUiScript>();
+            endScreen.setGameStats(this.toiletsFlushed, this.sinksFilled, this.secretRoomsFound);
+        };
         this.Sinks = FindObjectsOfType<DoctorsSink>();
 
         foreach (DoctorsSink sink in this.Sinks)
@@ -27,9 +48,15 @@ public class EasterEggManager : MonoBehaviour
         }
     }
 
+    public void reactToToiletFlushed()
+    {
+        this.toiletsFlushed = this.toiletsFlushed < 5 ? this.toiletsFlushed + 1 : 5;
+    }
+
     public void ReactToSinkFull()
     {
         this.fullSinkCount++;
+        this.sinksFilled = this.sinksFilled < 3? sinksFilled + 1: 3;
         if(this.fullSinkCount >= this.Sinks.Length)
             this.AllSinksFull();
     }
@@ -51,5 +78,12 @@ public class EasterEggManager : MonoBehaviour
     {
         this.shuttersOpen = false;
         this.CloseShuttersEvent.Invoke();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        this.secretRoomsFound = 1;
+
+        GetComponent<BoxCollider>().enabled = false;
     }
 }
